42d7cf30c31669152183aa9e3be92ec33e20ee07
Dmitry <dima11111993@yandex.ru>
2023-03-01 21:11:52 +0300

    basics_01

---
+++ basics/basics_01.py
@@ -0,0 +1,9 @@
+# Определить переменные всех 4 базовых типов и вывести их на экран
+my_int = 12
+print(my_int)
+my_float = 12.4
+print(my_float)
+my_str = "sdsadsaf34534r"
+print(my_str)
+my_bool = True
+print(my_bool)
