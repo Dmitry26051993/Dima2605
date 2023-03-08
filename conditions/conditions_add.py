ce9d12c7524b3bbc9d650f700afe304759f75f42
Dmitry <dima11111993@yandex.ru>
2023-03-08 19:38:21 +0300

    conditions_add

---
+++ conditions/conditions_add.py
@@ -0,0 +1,9 @@
+# Ввести имя. Если оно Борис, то поприветствовать.
+# Иначе нет.
+my_name = input("Введите Ваше имя: ")
+if my_name == "Borys":
+    print("Hello", my_name)
+elif my_name == "Борис":
+    print("Hello", my_name)
+else:
+    print("I don't know you")
