d5bb38ab3dcdae775213a528b6f16bfd9acb08af
Dmitry <dima11111993@yandex.ru>
2023-03-30 00:03:59 +0300

    got

---
+++ func_prog/func_prog_011.py
@@ -0,0 +1,17 @@
+# Создать декоратор для функции, которая принимает список чисел.
+# Декоратор должен производить предварительную проверку данных - удалять все четные элементы из списка.
+from random import randint
+def my_decorator(func):
+    def wrapper(arr):
+        print(arr)
+        new_arr = [i for i in arr if i % 2]
+        return func(new_arr)
+
+    return wrapper
+
+
+@my_decorator
+def real_func(x):
+    print(x)
+
+real_func([randint(1, 10) for i in range(10)])
--- func_prog/func_prog_08.py
+++ func_prog/func_prog_08.py
@@ -2,17 +2,26 @@
 # from datetime import datetime
 # time = datetime.now()
 from datetime import datetime
-def ineer(*args, **kwargs):
-    start_time = datetime.now()
-    result = func(*args, **kwargs)
-    finish_time = datetime.now()
-    delta = finish_time - start_time
-    print(delta)
-    return result
+from random import randint
 
+def measure_time(func):
+    def ineer(*args, **kwargs):
+        start_time = datetime.now()
+        result = func(*args, **kwargs)
+        finish_time = datetime.now()
+        delta = finish_time - start_time
+        print(delta)
+        return result
+    return ineer
+
+
+@measure_time
 def very_importnant_func(arr):
     arr = list(map(lambda x: x * 2, arr))
     print(arr)
     return arr
-arr = [i for i in range(10)]
+arr = [randint(-100, 1000) for i in range(1000)]
 very_importnant_func(arr)
+
+
+
