7e4161cf24e87a09e37e347d0c5c40d4f7469e98
Dmitry <dima11111993@yandex.ru>
2023-04-07 22:07:51 +0300

    add

--- files/files_10.py
+++ files/files_10.py
@@ -2,13 +2,45 @@
 # Создать отчетный файл с информацией по количеству людей входящих в ту или иную возрастную группу.
 # Возрастные группы: 1-12, 13-18, 19-25, 26-40, 40+.
 import csv
-from csv_utils import create, read
+arr = [["name", "lastname", "age"],
+       ["Alex", "Petrov", 25],
+       ["Sasha", "Morozov", 40],
+       ['Ivan', 'Ivanov', 15],
+       ["Zara", "Red", 12]]
+with open('test_10.csv', 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerows(arr)
 
-def main():
+dict = {'1-12': 0, '13-18': 0, '19-25': 0, '26-40': 0, '40+': 0}
+with open('test_10.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    rows = [row for row in csvreader]
+    for row in rows[1:]:
+        print(row)
+        age = (row[2])
+        if age < 13:
+            dict['1-12'] += 1
+        elif age < 19:
+            dict['13-18'] += 1
+        elif age < 26:
+            dict['19-25'] += 1
+        elif age < 41:
+            dict['26-40'] += 1
+        else:
+            dict['40+'] += 1
+fieldnames = ['1-12', '13-18', '19-25', '26-40', '40+']
 
-    fields = ["name", "lastname", "old"]
-    rows = [["Alex", "Petrov", 25], ["Sasha", "Morozov", 40], ["Zara", "Red", 12]]
-    create("test_3.csv", fields, rows)
-    print(read("test_3.csv"))
-if __name__ == '__main__':
-    main()
+with open('test_10_result.csv', 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(fieldnames)
+    arr = [dict['1-12'], dict['13-18'], dict['19-25'], dict['26-40']]
+    csvwriter.writerow(arr)
+
+with open('test_10_result.csv', 'r') as file:
+    csvreader = csv.DictReader(file)
+    for row in csvreader:
+        print(row)
+with open('test_10_result.csv', 'r') as file:
+    csvreader = csv.reader(file)
+    rows = [row for row in csvreader]
+    print(rows)
--- files/files_11.py
+++ files/files_11.py
@@ -1,18 +1,46 @@
 # Создать csv файл с данными о ежедневной погоде. Структура:  Дата, Место, Градусы, Скорость ветра.
 # Найти среднюю погоду(скорость ветра и градусы) для Минск за последние 7 дней.
-from csv_utils import create, read
-def main():
-    fields = ["date", "local", "temperature", "speed"]
-    rows = [
-        [15, "Minsk", 4, 15],
-        [16, "Minsk", 6, 21],
-        [17, "Minsk", -2, 25],
-        [18, "Minsk", 7, 9],
-        [19, "Minsk", 1, 5],
-        [20, "Minsk", 0, 7],
-        [21, "Minsk", 3, 18],
-    ]
-    create("test_4.csv", fields, rows)
-    print(read("test_4.csv"))
-if __name__ == '__main__':
-    main()
+import csv
+import datetime
+arr = [['date', 'place', 'degrees', 'wind'],
+       ['2023-04-5', 'minsk', 500, 1500],
+       ['2023-04-4', 'minsk', 10, 15],
+       ['2023-04-3', 'minsk', 5, 10],
+       ['2023-04-2', 'minsk', 9, 11],
+       ['2023-04-1', 'minsk', 15, 5],
+       ['2023-03-31', 'minsk', 12, 7],
+       ['2023-03-30', 'minsk', 14, 8],
+       ['2023-03-29', 'minsk', 8, 20],
+       ['2023-03-28', 'minsk', 8, 20],
+       ['2023-03-27', 'minsk', 10, 20],
+       ['2023-03-26', 'brest', 500, 1000],
+       ]
+
+delta_finish = datetime.datetime.now()
+delta = datetime.timedelta(7)
+delta_start = delta_finish - delta
+print(delta_start, delta_finish, int(delta.days))
+delta_start = delta_finish - datetime.datetime(year=2023, month=2, day=5)
+s = 1
+
+with open('test_11.csv', 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerows(arr)
+
+with open('test_11.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    rows = [row for row in csvreader]
+    for row in rows[1:]:
+        row[0] = datetime.datetime.strptime(row[0], '%Y-%m-%d')
+        row[2] = int(row[2])
+        row[3] = int(row[3])
+    total_degrees = 0
+    total_wind = 0
+    for row in rows[1:]:
+        if delta_start <= row[0] < delta_finish and row[1] == 'minsk':
+            total_degrees += row[2]
+            total_wind += row[3]
+    delta = int(delta.days)
+    avg_degrees = total_degrees / delta
+    avg_wind = total_wind / delta
+    print(f'avg_degrees : {avg_degrees}, avg_wind : {avg_wind}')
