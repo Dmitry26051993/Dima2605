fd048143f92a2a722852e72953651d764cff0f52
Dmitry26051993 <126193336+Dmitry26051993@users.noreply.github.com>
2023-05-25 01:07:24 +0300

    Update sqlalchemy_04.py

--- my_sqlalchemy/sqlalchemy_04.py
+++ my_sqlalchemy/sqlalchemy_04.py
@@ -1,11 +1,5 @@
-# Создать файл sqlalchemy_04.py.
-# Написать программу: пользователь вводит данные о книге.
-# Пользователю отображается введенная им информация.
-# Пользователю задается вопрос: “Хотите сохранить эту книгу?”.
-# Если ответ да - выполнить метод .commit(), иначе - .rollback()
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
-<<<<<<< HEAD
 
 engine = create_engine('sqlite:///sa_test.db')
 with engine.connect() as connection:
@@ -16,8 +10,19 @@ with engine.connect() as connection:
     price = float(input('price: '))
     releace_year = int(input('releace_year: '))
     try:
-        create_table_query = f"""insert into Book (title, pages,author, price, releace_year)values (:title, :pages, :author, :price, :releace_year);"""
-        ddl = text(create_table_query).bindparams(title=title, pages=pages, author=author, price=price, releace_year=releace_year)
+        create_table_query = f"""
+            insert into Book (title, pages,
+                  author, price, release_year)
+                  values (:title, :pages, 
+                  :author, :price, :releace_year);
+        """
+        ddl = text(create_table_query).bindparams(
+            title=title,
+            pages=pages,
+            author=author,
+            price=price,
+            releace_year=releace_year
+        )
         connection.execute(ddl)
         choise_trans = input(f'do you want to save book with '
                              f'id={id}, title={title}, pages={pages}, '
@@ -30,16 +35,3 @@ with engine.connect() as connection:
             transaction.rollback()
     except SQLAlchemyError as e:
         transaction.rollback()
-=======
-engine = create_engine('sqlite:///sa_test.db')
-with engine.connect() as connection:
-    transaction = connection.begin()
-    try:
-        create_table_query = """create table book (id integer primary key autoincrement, title varchar, pages int, author varchar, price float, releace_year int);"""
-        ddl = text(create_table_query)
-        connection.execute(ddl)
-        transaction.commit()
-    except SQLAlchemyError as e:
-        transaction.rollback()
-        print(f"Ошибка при выполнении SQL-запроса: {str(e)}")
->>>>>>> origin/my_sqlalchemy
